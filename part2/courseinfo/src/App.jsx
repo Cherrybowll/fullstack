4b6818e05e3d9992f1d47bbe09b037890cca8313
Cherrybowl <81250492+Cherrybowll@users.noreply.github.com>
2025-01-21 16:06:46 +0200

    p2e4

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -8,7 +8,7 @@ const Course = ({course}) =>
 const Header = ({name}) => <h1>{name}</h1>
 
 const Total = ({parts}) =>
-  <p>Total of {parts.reduce((sum, cur) => sum + cur.exercises, 0)} exercises</p>
+  <p><b>Total of {parts.reduce((sum, cur) => sum + cur.exercises, 0)} exercises</b></p>
 
 const Part = ({part}) => 
   <p>
@@ -21,29 +21,56 @@ const Content = ({parts}) =>
   </>
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
-  return <Course course={course} />
+  return (
+  <div>
+    {courses.map(course => <Course key={course.id} course={course} />)}
+  </div>
+  )
 }
 
 export default App
